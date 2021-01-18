836574802880c3bf40bd6493f3c68f3c41bab0c9
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-01-18 18:33:01 +0530

    Fix Typo

--- isAnOrA.js
+++ isAnOrA.js
@@ -3,7 +3,7 @@ const vowels = ['a', 'e', 'i', 'o', 'u']
 function isAorAn(word) {
 	
 	if (typeof word !== 'string') return false
-	if (vowel.includes(word.charAt(0).toLowerCase())) return 'a'
+	if (!vowels.includes(word.charAt(0).toLowerCase())) return 'a'
 	return 'an'
 	
 }
